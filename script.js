3bb45049362b2919f2b9567ccfab4d07e7991761
Jeffrey Chan <Jeffrey Chan>
2016-02-20 00:47:08 -0800

    I tried to get it to work, but it didnt

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
+    var REQUEST_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson';
+    var objectContainer = $('#container');
+    var responses = {
+    objects: [],
+    firstLoad: false,
+    hasError: false
+  };
+  var template = Handlebars.compile($('#objectTemplate').html());
+  $('#button').click(getObject);
+  renderObject();
 
+  function getObject() {
+    $.get(REQUEST_URL).then(function(resp) {
+      responses.objects.push(resp);
+      responses.hasError = false;
+    }).catch(function() {
+      responses.hasError = true;
+    }).always(function() {
+      responses.firstLoad = true;
+      renderInsult();
+    });
+  }
+
+  function renderObject() {
+    var templateHtml = template(responses);
+    objectContainer.empty()
+    objectContainer.html(templateHtml);
+  }
 });
